8056b797e4f5adc02f40d2974d282fa44113b304
AndreyDaynekq <124625955+AndreyDaynekq@users.noreply.github.com>
2023-02-14 13:42:43 +0300

    1

--- OAIP/laba1/mainZlata8.cpp
+++ OAIP/laba1/mainZlata8.cpp
@@ -3,7 +3,7 @@ using namespace std;
 // Найти максимальный элемент в массиве ai (i=1, 2, n), 
 // используя очевидное соотношение max(a1, a2, an) = max[max(a1, a2, an – 1), an].
 int max(int*, int);
-
+int max2(int*, int);
 int main()
 {
     int n;
@@ -17,7 +17,7 @@ int main()
     {
         cout << arr[i] << " ";
     }
-    cout << endl << max(arr, n);
+    cout << endl << max2(arr, n);
 }
 
 int max(int *arr, int n)
@@ -25,4 +25,18 @@ int max(int *arr, int n)
     if (n == 1) return *arr; 
     int count = n / 2; 
     return max(max(arr, count), max(arr + count, n - count));
+}
+int max2(int *arr, int n)
+{
+    if(n != 0)
+    {
+        if(max(arr, n - 1) < arr[n])
+    {
+        return arr[n];
+    }
+    else if(max(arr, n - 1) > arr[n])
+    {
+        return arr[n - 1];
+    }
+    }
 }
